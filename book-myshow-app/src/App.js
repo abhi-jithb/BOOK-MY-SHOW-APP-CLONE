b56069303cf1abc7ea355973c104035dae11fd0b
Abhijith B <babhijith58@gmail.com>
2024-02-03 14:45:39 +0000

    Initial Setup done

---
+++ book-myshow-app/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import "slick-carousel/slick/slick.css"; 
+import "slick-carousel/slick/slick-theme.css";
+import {Routes,Route} from "react-router-dom"
+function App() {
+  return (
+     <Routes>
+       {/* <Route path='/' element={<HomePage/>} />
+       <Route path='/movie/:id/' element={<MoviePage/>} />
+       <Route path='/plays' element={<Plays/>} /> */}
+
+      </Routes>
+  );
+}
+
+export default App;
