dbe7eea74115c467fa934abf1594ce65c2fec400
Abhijith B <babhijith58@gmail.com>
2024-02-03 15:12:24 +0000

    day1

--- book-myshow-app/src/App.js
+++ book-myshow-app/src/App.js
@@ -1,16 +1,15 @@
 import './App.css';
 import "slick-carousel/slick/slick.css"; 
 import "slick-carousel/slick/slick-theme.css";
-import {Routes,Route} from "react-router-dom"
+import {Routes,Route} from "react-router-dom";
 function App() {
   return (
      <Routes>
-       {/* <Route path='/' element={<HomePage/>} />
+        <Route path='/' element={<HomePage/>} />
        <Route path='/movie/:id/' element={<MoviePage/>} />
-       <Route path='/plays' element={<Plays/>} /> */}
-
-      </Routes>
+       <Route path='/plays' element={<Plays/>} /> 
+       </Routes>
   );
 }
 
-export default App;
+export default App();
