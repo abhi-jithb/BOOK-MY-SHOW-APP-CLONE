58e8c878f673ce3019c4e781fc132e9001e5abd5
Abhijith B <babhijith58@gmail.com>
2024-03-07 17:33:53 +0000

    t added some more code

--- book-myshow-app/src/App.js
+++ book-myshow-app/src/App.js
@@ -1,6 +1,8 @@
 import './App.css';
+import axios from "axios";
+import.env from "dotenv";
 // Router
-import {Routes,Route} from "react-router-dom"
+import { Routes, Route } from "react-router-dom"
 // React Slick
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
@@ -9,14 +11,18 @@ import HomePage from './pages/Home.page';
 import MoviePage from './pages/Movie.page';
 import PlayPage from './pages/Play.page';
 
+axios.defaults.baseURL = "https://api.themoviedb.org/3";
+axios.defaults.params = {};
+axios.defaults.params["api_key"] = process.env.REACT_APP_API_KEY;
+
 function App() {
   return (
-     <Routes>
-       <Route path='/' element={<HomePage/>} />
-       <Route path='/movie/:id/' element={<MoviePage/>} />
-       <Route path='/plays' element={<PlayPage/>} />
+    <Routes>
+      <Route path='/' element={<HomePage />} />
+      <Route path='/movie/:id/' element={<MoviePage />} />
+      <Route path='/plays' element={<PlayPage />} />
 
-      </Routes>
+    </Routes>
   );
 }
 
